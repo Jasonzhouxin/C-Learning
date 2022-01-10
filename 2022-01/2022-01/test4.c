ebc452b9450cb668912b3bac08d6623cf5a8bbe2
Jasonzhouxin <805331862@qq.com>
2022-01-11 00:10:01 +0800

    更新c文件20220111

--- 2022-01/2022-01/test4.c
+++ 2022-01/2022-01/test4.c
@@ -1,24 +1,29 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+#include<string.h>
 
 int main()
 {
-	char input;
-	char ch;
-	printf("请输入您的密码>：");
-	scanf("%s", &input);
-
-	while ((ch = getchar()) != '\n')
+	char password[30] = {0};
+	int i = 0;
+	for (i = 0; i < 3; i++)
 	{
-		putchar(ch);
-		//if (input == password[])
-		//{
-		//	printf("登录成功！\n");
-		//}
-		//else
-		//{
-		//	printf("登录失败！\n");
-		//}
+		printf("请输入您的密码>：");
+		scanf("%s", password);
+
+		if (strcmp(password, "123456") == 0)//字符串比较不能用==
+		{
+			printf("登录成功!\n");
+			break;
+		}
+		else
+		{
+			printf("密码错误！\n");
+		}
+	}
+	if (i == 3)
+	{
+		printf("三次密码输入错误，退出程序！\n");
 	}
 
 
