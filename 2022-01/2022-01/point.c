e57eb3285fee9e453d27b5a9bf406c418c33c352
Jasonzhouxin <805331862@qq.com>
2022-02-02 22:44:35 +0800

    更新C文件20220202

--- 2022-01/2022-01/point.c
+++ 2022-01/2022-01/point.c
@@ -1,15 +1,36 @@
 #include<stdio.h>
 
+//求字符串格式
+int my_strlen(char* str)
+{
+	char* start = str;
+	char* end = str;
+	while (*end != '\0')
+	{
+		end++;
+	}
+	return  end - start;
+}
+
 int main()
 {
-	int a = 10;
-	printf("%p\n", &a);
+	char arr[] = "bit";
 
-	int* p = &a;//&--取地址；p为指针变量，存放变量地址
-	printf("%p\n", p);
-
-	*p = 20;//*--解引用操作符
-	printf("%d\n", a);
-
+	int len = my_strlen(arr);
+	printf("%d", len);
 	return 0;
 }
+
+//int main()
+//{
+//	int a = 10;
+//	printf("%p\n", &a);
+//
+//	int* p = &a;//&--取地址；p为指针变量，存放变量地址
+//	printf("%p\n", p);
+//
+//	*p = 20;//*--解引用操作符
+//	printf("%d\n", a);
+//
+//	return 0;
+//}
