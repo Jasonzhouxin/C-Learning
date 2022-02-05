133c2ac473def4940a7b74ec9f290eff026d63a6
Jasonzhouxin <805331862@qq.com>
2022-02-06 00:25:23 +0800

    更新test6.c文件20220206

--- 2022-01/2022-01/test6.c
+++ 2022-01/2022-01/test6.c
@@ -1,33 +1,149 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 
-//写一个函数求a的二进制（补码）表示中有几个1
-count_bit_one(unsigned int n)
+//打印乘法口诀表
+void print_table(int n)
 {
-	int count = 0;
-	while (n)
+	int i = 0;
+	for (i = 1; i <= n; i++)
 	{
-		if (n % 2 == 1)
+		int j = 0;
+		for (j = 1; j <= i; j++)
 		{
-			count++;
+			printf("%d*%d=%-4d", i, j, i*j);
 		}
-		n = n / 2;
+		printf("\n");
 	}
-	return count;
-
 }
 
 int main()
 {
-	int a = 0;
-	scanf("%d", &a);
+	int n = 0;
+	scanf("%d", &n);
 
-	int count = count_bit_one(a);
-
-	printf("%d", count);
+	print_table(n);
 
 	return 0;
 }
+
+////打印二进制的奇数位和偶数位
+//void Print(int m)
+//{
+//	int i = 0;
+//	printf("奇数位：\n");
+//	for (i = 30; i >= 0; i -= 2)
+//	{
+//		printf("%d ", (m >> i) & 1);
+//	}
+//	printf("\n");
+//
+//	printf("偶数位：\n");
+//	for (i = 31; i >= 1; i -= 2)
+//	{
+//		printf("%d ", (m >> i) & 1);
+//	}
+//	printf("\n");
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//
+//	Print(n);
+//
+//	return 0;
+//}
+
+
+//求二进制中不同位的个数
+//count_bit_diff(int m, int n)
+//{
+//	int tmp = m ^ n;
+//	return count_bit_one(tmp);
+//}
+//
+//
+////count_bit_diff(int m, int n)
+////{
+////	int tmp = m ^ n;
+////	int i = 0;
+////	int count = 0;
+////	for (i = 0; i < 32; i++)
+////	{
+////		if (((tmp >> i) & 1) == 1)
+////		{
+////			count++;
+////		}
+////	}
+////	return count;
+////}
+//
+//int main()
+//{
+//	int m = 0;
+//	int n = 0;
+//	scanf("%d %d", &m,&n);
+//
+//	int count = count_bit_diff(m, n);
+//
+//	printf("%d\n", count);
+//
+//	return 0;
+//}
+//
+////写一个函数求a的二进制（补码）表示中有几个1
+//count_bit_one(int n)
+//{
+//	int count = 0;
+//
+//	while (n)
+//	{
+//		n = n & (n - 1);
+//		count++;
+//	}
+//	return count;
+//}
+
+//count_bit_one(int n)
+//{
+//	int count = 0;
+//	int i = 0;
+//	for (i = 0; i < 32; i++)
+//	{
+//		if ((n >> i) & 1 == 1)
+//		{
+//			count++;
+//		}
+//	}
+//	return count;
+//}
+
+//count_bit_one(unsigned int n)//函数形参接收无符号整型数
+//{
+//	int count = 0;
+//	while (n)
+//	{
+//		if (n % 2 == 1)
+//		{
+//			count++;
+//		}
+//		n = n / 2;
+//	}
+//	return count;
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//
+//	int count = count_bit_one(a);
+//
+//	printf("%d", count);
+//
+//	return 0;
+//}
 
 //int main()
 //{
